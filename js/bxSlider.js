31dfc1b9677238241a0ed29e2f05373920246428
asiri <asiri@leapset.com>
2018-04-17 03:52:13 +0530

    Modified to cater hot res

--- js/bxSlider.js
+++ js/bxSlider.js
@@ -1,6 +1,6 @@
 /**
  * bxSlider v4.2.12
- * Copyright 2013-2015 Steven Wanderski
+ * Copyright 2013-2018 Steven Wanderski
  * Written while drinking Belgian ales and listening to jazz
  * Licensed under MIT (http://opensource.org/licenses/MIT)
  */
